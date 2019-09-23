15fad6b82403aebc11888ac23ea141dc3ac0e6b6
Cristian Javier Alvarez <cjavi26000@hotmail.com>
2019-09-23 15:29:36 -0600

    Actualizar hecho
    Ya esta la funcion de actulizar pero el problema es que el diseño aun falta arreglarlo. Pero las funciones basicas del CRUD ya estan hechas ahora solo toca arreglarlo esticamente y ver el problema de los videos embebidos.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,7 @@ export class AppComponent {
   public text1:String = '';
   public text2:String =  '';
   public text3: String = '';
+  public text4: String = '';
   public idEliminar: String = '';
 
   ngOnInit():void {
@@ -37,6 +38,16 @@ export class AppComponent {
     for(var i = 0; i < this.data.length; i++) {
       if(this.data[i].personaje === idEliminar) {
          this.data.splice(i, 1);
+      }
+  }
+  }
+
+  updateElement(text1: String, text2: String, text3: String, text4: String) {
+    for(var i = 0; i < this.data.length; i++) {
+      if(this.data[i].personaje === text1) {
+         this.data[i].personaje = text2;
+         this.data[i].franquicia = text3;
+         this.data[i].descripcion = text4;
       }
   }
   }
