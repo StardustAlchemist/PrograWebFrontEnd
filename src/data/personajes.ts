e4a066c3d837daa6cfb0a7ebe659d5fd78f31142
Cristian Javier Alvarez <cjavi26000@hotmail.com>
2019-09-23 12:54:32 -0600

    Creacion de Tabla en la Vista y local storage

---
+++ src/data/personajes.ts
@@ -0,0 +1,23 @@
+export const storage:any = [
+    {
+        Personaje: 'Mario',
+        Franquicia: 'Super Mario',
+        Imagen: 'No hay Imagen',
+        Descripcion: 'Es un plomero italiano hero del reino chapiñon',
+        Video: ''
+    },
+    {
+        Personaje: 'Kirby',
+        Franquicia: 'Kirby',
+        Imagen: 'No hay Imagen',
+        Descripcion: 'Hero de DreamLand',
+        Video: 'https://www.youtube.com/embed/1FPJs_c3qY4'
+    },
+    {
+        Personaje: 'Fox',
+        Franquicia: 'Star Fox',
+        Imagen: 'No hay Imagen',
+        Descripcion: 'Lider del Equipo Star Fox',
+        Video: 'https://www.youtube.com/embed/Arw6c0dJHAE'
+    }
+]
